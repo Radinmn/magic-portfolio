7aeab64d543b8af2919645be2c2f2a6ca5410f29
Radin <radinmoayyedian@gmail.com>
2026-04-10 01:50:08 -0400

    update

--- src/components/work/Projects.tsx
+++ src/components/work/Projects.tsx
@@ -32,7 +32,6 @@ export function Projects({ range, exclude, compact = false }: ProjectsProps) {
         <ProjectCard
           priority={index < 2}
           key={post.slug}
-          // href={`/work/${post.slug}`}
           images={post.metadata.images}
           title={post.metadata.title}
           description={post.metadata.summary}
